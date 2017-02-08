c462e4691caae704ead9fe41bb0fcea986620069
Ryan Stecher <rgs030@bucknell.edu>
2017-02-07 20:14:41 -0500

    Moved API key to environment var

--- server/controllers/hackerRank.controller.js
+++ server/controllers/hackerRank.controller.js
@@ -1,6 +1,6 @@
 import request from 'request';
 
-const API_KEY = 'hackerrank|1761054-1191|25bcc1c3bcaeafaffc31a072b0a9ff725533893c';
+const API_KEY = process.env.HR_API_KEY;
 const LANG_CODE = {'fsharp': 33, 'javascript': 20, 'whitespace': 41, 'python': 5, 'lolcode': 38, 'mysql': 10, 'fortran': 54, 'tcl': 40, 'oracle': 11, 'pascal': 25, 'haskell': 12, 'cobol': 36, 'octave': 46, 'csharp': 9, 'go': 21, 'php': 7, 'ruby': 8, 'java8': 43, 'bash': 14, 'visualbasic': 37, 'groovy': 31, 'c': 1, 'erlang': 16, 'java': 3, 'd': 22, 'scala': 15, 'tsql': 42, 'ocaml': 23, 'perl': 6, 'lua': 18, 'xquery': 48, 'r': 24, 'swift': 51, 'sbcl': 26, 'smalltalk': 39, 'racket': 49, 'cpp': 2, 'db2': 44, 'objectivec': 32, 'clojure': 13, 'python3': 30, 'rust': 50};
 
 /**
