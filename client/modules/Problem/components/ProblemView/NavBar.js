897db0a821b80a79461dfb13a28eabb3b59d4639
Andrew Wheeler <asw011@bucknell.edu>
2017-02-27 10:44:34 -0500

    Created component using npm package to render PDFs

--- client/modules/Problem/components/ProblemView/NavBar.js
+++ client/modules/Problem/components/ProblemView/NavBar.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import ProblemButton from './ProblemButton.js';
-import PDF from 'react-pdf-js';
 import {fetchProblem} from '../../../Contests/ContestActions';
+import spdf from "simple-react-pdf";
 
 class NavBar extends React.Component {
 
     constructor() {
         super();
-        this.state = {
-            prompt_text: this.props.competition.problems[0].prompt,
-            sample_input: this.props.competition.problems[0].input,
-            sample_output: this.props.competition.problems[0].output
-        };
+        this.state = {};
+        // this.state = {
+        //     prompt_text: this.props.competition.problems[0].prompt,
+        //     sample_input: this.props.competition.problems[0].input,
+        //     sample_output: this.props.competition.problems[0].output
+        // };
     }
 
     componentDidMount() {
         fetchProblem("cikqgkv4q01ck7453ualdn3hn", "1").then(response => {
             response.blob().then(blob => {
                 const pdf = new File([blob], "problem.pdf");
-                this.setState({pdf});
+                const pdfUrl = URL.createObjectURL(pdf);
+                this.setState({pdfUrl});
             });
         });
     }
@@ -36,25 +38,26 @@ class NavBar extends React.Component {
     }
 
     render() {
-        const handleClick = this.handleClick;
-        const problem_list = this.props.competition.problems;
-        const team = this.props.competition.teams[0];
-        const pdf = this.state.pdf ?
-            (<PDF file={this.state.pdf} page={1}/>) : null;
+        //const handleClick = this.handleClick;
+        //const problem_list = this.props.competition.problems;
+        //const team = this.props.competition.teams[0];
+        // {Object.keys(problem_list).map((key) => {
+        //     return <ProblemButton key={key}
+        //     title={problem_list[key].title}
+        //     prompt={problem_list[key].prompt}
+        //     sample_input={problem_list[key].input}
+        //     sample_output={problem_list[key].output}
+        //     status={team.status[key]}
+        //     onChange={handleClick} />
+        // })}
+        // <div>{this.state.prompt_text}</div>
+        // <div>{this.state.sample_input}</div>
+        // <div>{this.state.sample_output}</div>
+        const pdf = this.state.pdfUrl ?
+            (<spdf.SimplePDF file={this.state.pdfUrl}/>) : null;
         return (
             <div>
-                {Object.keys(problem_list).map((key) => {
-                    return <ProblemButton key={key}
-                    title={problem_list[key].title}
-                    prompt={problem_list[key].prompt}
-                    sample_input={problem_list[key].input}
-                    sample_output={problem_list[key].output}
-                    status={team.status[key]}
-                    onChange={handleClick} />
-                })}
-                <div>{this.state.prompt_text}</div>
-                <div>{this.state.sample_input}</div>
-                <div>{this.state.sample_output}</div>
+                <input type="file"></input>
                 {pdf}
             </div>
         );
