4efbf7096b5cfa9e7475fc2176705b1f9667c4bb
Andrew Wheeler <asw011@bucknell.edu>
2017-02-26 19:13:09 -0500

    Couldn't get server running because of navigator issue

--- client/modules/Problem/components/ProblemView/NavBar.js
+++ client/modules/Problem/components/ProblemView/NavBar.js
@@ -1,71 +1,64 @@
-/**
- * Created by courtneybolivar on 11/16/16.
- */
 import React from 'react';
 import ProblemButton from './ProblemButton.js';
 import PDF from 'react-pdf-js';
+import {fetchProblem} from '../../../Contests/ContestActions';
 
-/**
- *
- * @type {*}
- */
-var NavBar = React.createClass({
+class NavBar extends React.Component {
 
-  /*
-    sets the initial states to be the first problem in the list
-   */
-  getInitialState: function () {
-    return {
-      prompt_text: this.props.competition.problems[0].prompt,
-      sample_input: this.props.competition.problems[0].input,
-      sample_output: this.props.competition.problems[0].output
-    };
-  },
+    constructor() {
+        super();
+        this.state = {
+            prompt_text: this.props.competition.problems[0].prompt,
+            sample_input: this.props.competition.problems[0].input,
+            sample_output: this.props.competition.problems[0].output
+        };
+    }
 
-  /**
-   * takes in the text to display. Called from ProblemButton.js
-   * @param text
-   */
-  handleClick: function (prompt, sample_input, sample_output){
-    this.setState({
-      prompt_text: prompt,
-      sample_input: sample_input,
-      sample_output: sample_output
-    });
-  },
+    componentDidMount() {
+        fetchProblem("cikqgkv4q01ck7453ualdn3hn", "1").then(response => {
+            response.blob().then(blob => {
+                const pdf = new File([blob], "problem.pdf");
+                this.setState({pdf});
+            });
+        });
+    }
 
-  /*
+    handleClick(prompt, sample_input, sample_output) {
+        this.setState({
+            prompt_text: prompt,
+            sample_input: sample_input,
+            sample_output: sample_output
+        });
+    }
 
-   */
-  _onPdfCompleted: function(page, pages){
-    this.setState({page: page, pages: pages});
-  },
+    _onPdfCompleted(page, pages) {
+        this.setState({page: page, pages: pages});
+    }
 
-  /*
-    creates the buttons to allow problem navigation.
-   */
-  render: function () {
-    const handleClick = this.handleClick;
-    const problem_list = this.props.competition.problems;
-    const team = this.props.competition.teams[0];
-    return (
-      <div>
-          {Object.keys(problem_list).map(function(key){
-            return <ProblemButton key={key}
-                                  title={problem_list[key].title}
-                                  prompt={problem_list[key].prompt}
-                                  sample_input={problem_list[key].input}
-                                  sample_output={problem_list[key].output}
-                                  status={team.status[key]}
-                                  onChange={handleClick} />
-          })}
-          <div>{this.state.prompt_text}</div>
-          <div>{this.state.sample_input}</div>
-          <div>{this.state.sample_output}</div>
-          <PDF file="./pdf_problem.pdf" page={2}/>
-      </div>
-    );
-  }
-});
+    render() {
+        const handleClick = this.handleClick;
+        const problem_list = this.props.competition.problems;
+        const team = this.props.competition.teams[0];
+        const pdf = this.state.pdf ?
+            (<PDF file={this.state.pdf} page={1}/>) : null;
+        return (
+            <div>
+                {Object.keys(problem_list).map((key) => {
+                    return <ProblemButton key={key}
+                    title={problem_list[key].title}
+                    prompt={problem_list[key].prompt}
+                    sample_input={problem_list[key].input}
+                    sample_output={problem_list[key].output}
+                    status={team.status[key]}
+                    onChange={handleClick} />
+                })}
+                <div>{this.state.prompt_text}</div>
+                <div>{this.state.sample_input}</div>
+                <div>{this.state.sample_output}</div>
+                {pdf}
+            </div>
+        );
+    }
+}
 
 export default NavBar;
