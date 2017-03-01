95c9c195dafa5ecd8eb861c82c4a7f9532d5e4d7
Courtney Bolivar <clb057@bucknell.edu>
2017-03-01 15:21:33 -0500

    Scoreboard working with database

--- client/modules/Scoreboard/components/BarChart.js
+++ client/modules/Scoreboard/components/BarChart.js
@@ -1,73 +1,36 @@
 const React = require('react');
 const {Bar} = require("react-chartjs");
+// example/source originally taken from: https://ledsun.github.io/react-chartjs-example/bar/
 
-// https://ledsun.github.io/react-chartjs-example/bar/
 const options = {
-  // Elements options apply to all of the options unless overridden in a dataset
-  // In this case, we are setting the border of each bar to be 2px wide and green
-  elements: {
-    rectangle: {
-      borderWidth: 2,
-      borderColor: '#ff6384',
-      borderSkipped: 'bottom'
-    }
-  },
-  responsive: true,
-  legend: {
-    display: true,
-    position: 'top'
-  },
-  showTooltips: false, // disables tool tips because they didn't update with new data
+  responsive: true, // makes it fill page
+  showTooltips: false, // disables tool tips
 };
 
-
+/**
+ * holds the data passed in from the page and creates a bar chart
+ */
 var BarChart = React.createClass({
   getInitialState() {
     return {
-      datasets: [ // could add more datasets in this list if we wanted to list problems separately in the score board
-                  // since we probably don't want to do this just add each score for the team in the data field
+      datasets: [ // could add more data sets if you wanted more than addtional bars per team for other info
         {
           label: "Score", // "The label for the dataset which appears in the legend and tooltips"
-          fillColor: "rgba(220,220,220,0.5)",
-          data: this.props.scores
+          fillColor: "rgba(178,225,102,0.5)", // color for the bars
+          data: this.props.scores // values for each bar
         }
       ],
-      labels: this.props.names // labels for the bars
+      labels: this.props.names // labels for the bars (team names)
     }
   },
 
-  randmizeData() {
-    this.props.update(); // call the update method from Scoreboard.js.. this calls fetchScoreboardData, sets the states
-                         // in scoreboard.js causing a rerender of the page, this will rerender the barchart.. eg sending
-                         // those new states from the data base into this class, so we can reset the states from the props now
-    this.setState({
-      datasets: [
-        {
-          //label: "Problems Solved",
-          fillColor: "rgba(220,220,0,0.5)",
-          data: this.props.scores
-        }
-      ],
-      labels: this.props.names
-    });
-    console.log("in bar chart, scores = " + this.state.scores);
-    //this.forceUpdate();
-  },
-
   /**
-   * called before a render()
+   * creates the bar chart with the data given
+   * @returns {XML}
    */
-  componentWillMount() {
-    console.log("in bar chart component will mount");
-    this.randmizeData();
-  },
-
   render() {
-    console.log("in bar chart render props = " + this.props.names);
-    this.props.update();
     return <div>
       <Bar type='bar' data={this.state} options={options} ref={(ref) => this.Bar = ref}/>
-      <button onClick={this.randmizeData}>Update Data</button>
     </div>
   }
 });
--- client/modules/Scoreboard/pages/ScoreboardPage.js
+++ client/modules/Scoreboard/pages/ScoreboardPage.js
@@ -6,71 +6,50 @@ import React from 'react';
 import BarChart from '../components/BarChart.js';
 import {fetchScoreboardData} from '../../Contests/ContestActions.js';
 
-//TODO: add prop to allow judge to hide or show scoreboard?
 export default class ScoreBardPage extends React.Component {
 
-
+  /**
+   * sets the contest id, other states to be set later
+   * @param props
+   */
   constructor(props) {
     super(props);
-/*
-    var contest_id = "cikqgkv4q01ck7453ualdn3hn";  // doesn't work in constructor
-    var labels;
-    var scores;
-    fetchScoreboardData(contest_id).then(res => {
-      labels = res.teams.teamNames;
-      scores = res.teams.teamScores;
-    }); */
-
-    this.state = { // dummy info initially...
-      labels: ["hello", "hi", "hey"],
-      scores: [3, 3, 3],
+    this.state = {
       contest_id: "cikqgkv4q01ck7453ualdn3hn"
     };
-
   }
 
   /**
-   * set state in this method does not cause a rerender, because this is called right before a render
+   * called right before a render, gets the info from the database
    */
   componentWillMount(){
-    var done = "";
     fetchScoreboardData(this.state.contest_id).then(res => {
       this.setState({
           labels: res.teams.teamNames,
           scores: res.teams.teamScores
       })
-      done = "done!"
     });
-    console.log("updating names and scores? " , done);
-    console.log("team names: " + this.state.labels);
-    console.log("team scores: " + this.state.scores);
     this.render();
   }
 
-  update(){
-    this.render();
-    console.log("in scoreboard.update");
-  }
-
+  /**
+   * creates a bar chart with the data
+   * Later: Could add info on the contest like time left, current winner, etc.
+   *        Also, ability to show or hide the scoreboard
+   *        contest_id passed in as a prop
+   * @returns {XML}
+   */
   render() {
-      // make contest id a state and then change the state and it will rerender
-    /*
-     this.setState({
-     contest_id: "cikqgkv4q01ck7453ualdn3hn"
-     })
-     */
-    //this.state.contest_id = "cikqgkv4q01ck7453ualdn3hn";
-
-
-
     const scores = this.state.scores;
     const labels = this.state.labels;
 
+    // this ensures that the bar chart will not show until the data is grabbed from the data base
+    const Scoreboard = this.state.scores ? <BarChart names={this.state.labels} scores={this.state.scores}/> : null;
     return (
       <div>
         <h1> Welcome to the Scoreboard page </h1>
         <br/>
-        <BarChart names={this.state.labels} scores={this.state.scores} update ={this.update.bind(this)}/>
+        {Scoreboard}
       </div>
     );
   }
