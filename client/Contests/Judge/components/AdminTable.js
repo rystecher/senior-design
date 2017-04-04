ebfeca91742304f2d0dd20ffb7eaaa106d7197dc
Courtney Bolivar <clb057@bucknell.edu>
2017-04-04 00:33:25 -0400

    Using react-table for Submissions table

--- client/Contests/Judge/components/AdminTable.js
+++ client/Contests/Judge/components/AdminTable.js
@@ -2,10 +2,11 @@
  * Created by courtneybolivar on 21/02/2017.
  */
 import React from 'react';
-import Submission from './Submission.js';
-import Styles from './Admin.css';
-
+import { withRouter } from 'react-router';
 import {fetchSubmissions} from '../../ContestActions.js';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import '../../Home/pages/display-contests.css';
 
 const AdminTable = React.createClass({
 
@@ -31,33 +32,59 @@ const AdminTable = React.createClass({
     });
   },
 
+  goProblemView(cuid) {
+    this.props.router.push(`/contest/${this.state.contestID}/submissions/${cuid}`);
+  },
+
   /**
    *
    * @returns {XML}
    */
    render() {
 
+    const columns = [{
+      header: 'Team Name',
+      accessor: 'teamName'
+    }, {
+      header: 'Problem Name',
+      accessor: 'problemName'
+    }, {
+      header: 'Feedback',
+      accessor: 'feedback'
+    }];
+
     const submissions = this.state.submissions;
-    let contestSubmissions  = null; // if we've fetched submissions, contestSubmissions
-                                    // will be an array of <Submission > rows for the table
+    let data  = [];
     if(this.state.submissions.length > 0) {
-      contestSubmissions = submissions.map((sub) => <Submission sub={sub} key={sub.cuid}/>);
+      submissions.forEach((submission) => {
+        data.push({
+          teamName: submission.teamName,
+          problemName: submission.problemName,
+          feedback: submission.feedback,
+          cuid: submission.cuid
+        });
+      });
     }
 
     return (
       <div>
-        <table className={Styles}>
-          <tr>
-            <th>Team</th>
-            <th>Problem Name</th>
-            <th>Correct?</th>
-            <th>Feedback Given</th>
-          </tr>
-          {contestSubmissions}
-        </table>
+        <ReactTable
+          data={data}
+          columns={columns}
+          showPageSizeOptions={false}
+          defaultPageSize={10}
+          className='-highlight'
+          getTdProps={(state, rowInfo, column, instance) => {
+            return {
+              onClick: e => {
+                this.goProblemView(rowInfo.row.cuid);
+              },
+            };
+          }}
+        />
       </div>
     );
   }
 });
 
-export default AdminTable;
+export default withRouter(AdminTable);
